79ea1f7c3b66470f7e47a47160d855b2bfcd69d4
gourab-sinha <gourab19964u@gmail.com>
2020-08-28 01:17:40 +0530

    mathobjects

---
+++ js_sandbox/mathobjects.js
@@ -0,0 +1,58 @@
+const num1 = 100;
+const num2 = 50;
+let val;
+
+// Simple math with numbers
+val = num1 + num2;
+console.log(val);
+
+val = num1 - num2;
+console.log(val);
+
+val = num1 * num2;
+console.log(val);
+
+val = num1 / num2;
+console.log(val);
+
+val = num1 % num2;
+console.log(val);
+
+// Math object
+
+val = Math.PI  // Pie
+console.log(val.toFixed(2)); // Decimal points to fixed "toFixed"
+
+val = Math.E // Euler Number
+console.log(val.toFixed(2)); // Decimal points to fixed "toFixed"
+
+val = Math.round(2.4) // Round value
+console.log(val); 
+
+val = Math.ceil(2.4) // Ceiling value
+console.log(val.toFixed(2)); // Decimal points to fixed "toFixed"
+
+val = Math.floor(2.8); // Floor value
+console.log(val.toFixed(2)); // Decimal points to fixed "toFixed"
+
+val = Math.sqrt(64) // Sqrt of a number
+console.log(val); 
+
+val = Math.abs(-1) // Absolute value
+console.log(val); // Decimal points to fixed "toFixed"
+
+val = Math.pow(8,2); // Power function
+console.log(val);
+
+val = Math.min(23,34,45,5,6,23,-23,12); // Min
+console.log(val);
+
+val = Math.max(23,34,45,5,6,23,-23,12); // Max
+console.log(val);
+
+val = Math.floor(Math.random()*20 +1); // Random
+console.log(val);
+
+
+
+
