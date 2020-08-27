c414cd6c7509497a2615b954a57e04edfff3c992
gourab-sinha <gourab19964u@gmail.com>
2020-08-28 00:13:05 +0530

    variable and data type

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -17,3 +17,50 @@ console.time();
     console.log('Check time');
 console.timeEnd(); // time taken to execute
 
+// Variable and data types
+
+var name = "Gourab Sinha";
+console.log(name);
+name = "Sinha Gourab"; // reassignment
+console.log(name);
+
+// Init var
+var greeting;
+console.log(greeting)
+greeting = "Hello";
+console.log(greeting);
+
+// letters, numbers, _, $ can be used as starting of variable name
+// Cannot start with number
+
+var _name = 'John'; // used for private
+console.log(_name);
+
+// Multi word vars
+var firstName = "Gourab";
+var first_name = "Gourab";
+var FirstName = "Tom";
+var firstname;
+
+// let and var works as same
+
+const firstNameConst = 'Gourab'; // cannot define without initializing it
+console.log(firstNameConst);
+
+// object values can be altered but cannot reassigned with new values.
+const person = {
+    name: 'John',
+    age: 30
+}
+
+/* This is not possible
+person = {
+    firstName: 'Gourab',
+    lastName: 'Sinha'
+}
+console.log(person)
+
+
+console.log(person)
+*/
+
