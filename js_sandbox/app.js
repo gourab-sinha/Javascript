998454fa6364a47a7cb6bea04d77e568f9552780
gourab-sinha <gourab19964u@gmail.com>
2020-08-27 20:47:54 +0530

    Getting started with console and javascript file.

---
+++ js_sandbox/app.js
@@ -0,0 +1,19 @@
+// single line comment
+/*
+    Multiline comment
+*/
+console.log('Hello world');
+console.log(123);
+console.log(true);
+var greeting = 'hello';
+console.log(greeting);
+console.log([1,2,3,4]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+console.error('Error message');
+console.clear(); // clears the screen
+console.warn('The warning message!');
+console.time();
+    console.log('Check time');
+console.timeEnd(); // time taken to execute
+
