c94cb0be7eae0beb8db42dccb4c7da155d1e704b
gourab-sinha <gourab19964u@gmail.com>
2020-08-28 00:37:10 +0530

    datatypes

---
+++ js_sandbox/datatypes.js
@@ -0,0 +1,57 @@
+// Primitive data types
+// String sequence of character
+const name = "John Doe";
+console.log(name);
+console.log(typeof name);
+
+// Number
+const age = 45
+console.log(age)
+console.log(typeof age);
+
+// Boolean
+const hasKids = true;
+console.log(hasKids);
+console.log(typeof hasKids)
+
+// Null
+const car = null;
+console.log(car);
+console.log(typeof car); // Issue with javascript
+
+// Undefined
+let test;
+console.log(test);
+console.log(typeof test);
+
+// Symbols (ES6)
+const symbol = Symbol();
+console.log(symbol);
+console.log(typeof symbol);
+
+
+// Reference data types
+
+// Array
+const hobbies = ["movies", "music"];
+console.log(hobbies);
+console.log(typeof hobbies);
+
+// Object Literals
+const address = {
+    city: "Boston",
+    state: 'MA'
+}
+console.log(address);
+console.log(typeof address)
+
+// Functions
+// Dates
+const today = new Date();
+console.log(today);
+console.log(typeof today);
+
+// Anything else
+
+// Types are associated with values not variables
+// The same variable can hold multiple types
