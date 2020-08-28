ae730d9be0dfb8f7fe2690c96d4a70733be9e0a6
gourab-sinha <gourab19964u@gmail.com>
2020-08-29 01:16:46 +0530

    window object

---
+++ js_sandbox/windowobject.js
@@ -0,0 +1,93 @@
+// Window methods  // Objects // Properties
+console.log(12);
+
+// Alert
+// alert("Hello world");
+
+// Prompt
+// const input = prompt();
+// alert(input);
+
+// Confirm
+// if(confirm('Are you sure')){
+//     console.log('Yes');
+// }
+// else{
+//     console.log('No');
+// }
+
+// Height width
+let val
+
+// outter height and width
+val = window.outerHeight;
+console.log(val);
+
+val = window.outerWidth;
+console.log(val);
+
+// inner height and width
+val = window.innerHeight;
+console.log(val);
+
+val = window.innerWidth;
+console.log(val);
+
+// Scroll points
+val = window.scrollY; // Vertical
+console.log(val);
+
+val = window.scrollX; // Horizontal
+console.log(val);
+
+// Location object
+val = window.location;
+console.log(val);
+
+val = window.location.hostname;
+console.log(val);
+
+val = window.location.port;
+console.log(val);
+
+val = window.location.href;
+console.log(val);
+
+val = window.location.search;
+console.log(val);
+
+// Redirect
+// window.location.href = "https://google.com";
+// console.log(val);
+
+// Reload
+// window.location.reload();
+console.log(val);
+
+// History Object
+
+// window.history.go(-2);
+val = window.history.length;
+console.log(val);
+
+// Navigator object
+val = window.navigator;
+console.log(val);
+
+val = window.navigator.appName;
+console.log(val);
+
+val = window.navigator.appVersion;
+console.log(val);
+
+val = window.navigator.userAgent;
+console.log(val);
+
+val = window.navigator.platform;
+console.log(val);
+
+val = window.navigator.vendor;
+console.log(val);
+
+val = window.navigator.language;
+console.log(val);
